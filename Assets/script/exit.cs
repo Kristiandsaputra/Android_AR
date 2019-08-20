80665bdb3dd892de5b5217f5a025c47ddd0d0083
Kristiandsaputra <bsmbangs@gmail.com>
2019-08-20 13:02:53 +0700

    AR

---
+++ Assets/script/exit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class exit : MonoBehaviour {
+
+    public void doExitGame()
+    {
+        Application.Quit();
+    }
+}
